ee35c2bf16258ffa083dad149f2c2f0b2ceec1b1
Bronzato1 <thierry.langie@skynet.be>
2020-04-19 19:11:24 +0200

    Some changes

---
+++ CoursePlus/Client/Components/HeaderBase.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Components;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CoursePlus.Client.Components
+{
+    public class HeaderBase : ComponentBase
+    {
+        [Parameter]
+        public string Class { get; set; }
+
+        [Parameter]
+        public string UkSticky { get; set; }
+    }
+}
